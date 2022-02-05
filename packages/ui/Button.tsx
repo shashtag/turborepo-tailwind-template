888b40ea0f298a4f40fb0c2d228a45e874ff9133
shahtag <shashwatsatna@gmail.com>
2022-02-06 03:42:21 +0530

    🚧 tailwind global theme

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,6 +1,9 @@
 export const Button = () => {
   return (
-    <button type="button" className="h-16 w-16 bg-blue-500 hover:bg-blue-700">
+    <button
+      type="button"
+      className="rounded-4xl h-16 w-16 bg-blue-900 hover:bg-blue-700"
+    >
       Boop
     </button>
   )
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,7 +1,19 @@
+const defaultTheme = require('config/defaultTailwind.config')
+
 module.exports = {
   content: ['./**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    // ...defaultTheme.theme,
+
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
 }
