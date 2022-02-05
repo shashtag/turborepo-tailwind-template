d0dee104d5422ac82f85416bc66d525e28899139
shahtag <shashwatsatna@gmail.com>
2022-02-06 04:07:02 +0530

    ✨ tailwind theme setup

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -2,7 +2,7 @@ export const Button = () => {
   return (
     <button
       type="button"
-      className="rounded-4xl h-16 w-16 bg-blue-900 hover:bg-blue-700"
+      className="rounded-8xl h-16 w-16 bg-blue-900 hover:bg-blue-700"
     >
       Boop
     </button>
