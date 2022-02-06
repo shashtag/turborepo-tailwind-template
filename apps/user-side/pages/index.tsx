94e6b56b0859d18ee8a9f01ea5bcff96f9729c09
shahtag <shashwatsatna@gmail.com>
2022-02-06 12:58:50 +0530

    ✨ tailwind demo

--- apps/user-side/pages/index.tsx
+++ apps/user-side/pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-
-
+import { Button } from 'ui/Button'
 const Home = () => (
   <div className="flex min-h-screen flex-col items-center justify-center py-2">
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
+    <Button />
   </div>
 )
 
